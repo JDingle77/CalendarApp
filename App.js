07bc8138dedacc5553d2b7a823cf01bf01b85b4c
James Dingle <jdingle7@g.ucla.edu>
2021-02-18 21:05:02 -0800

    added 2021 and Jan

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.ycontainer} >
+        <Text style={styles.year}>2021</Text>
+      </View>
+      <View style={styles.mcontainer}>
+        <Text style={styles.month}>Jan</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+  ycontainer: {
+    flex: 3,
+    backgroundColor: '#000',
+    marginTop: 50,
+    marginLeft: 20
+  },
+  year: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 35,
+  },
+  month: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  mcontainer: {
+    flex: 30,
+    backgroundColor: '#000',
+    marginLeft: 20
+  }
 });
